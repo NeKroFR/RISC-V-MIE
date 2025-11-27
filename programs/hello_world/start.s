493137e518b14fddac2ceacb767d857e7a3d4e9b
NeKroFR <70316327+NeKroFR@users.noreply.github.com>
2025-11-27 10:16:03 +0100

    refine elf structure

--- programs/hello_world/start.s
+++ programs/hello_world/start.s
@@ -1,12 +1,12 @@
 .section .text.start
 .global _start
- .global main
+.global main
 
 _start:
-    # Set up stack pointer (64 KB)
-    li sp, 0x00010000
+    # Set up stack at top of 64 KiB DMEM (0x80010000)
+    li sp, 0x80010000
 
-    # Clear BSS
+    # Clear BSS (0x8000xxxx range)
     la a0, _bss_start
     la a1, _bss_end
 bss_loop:
@@ -15,5 +15,4 @@ bss_loop:
     addi a0, a0, 4
     j bss_loop
 bss_done:
-
     call main
